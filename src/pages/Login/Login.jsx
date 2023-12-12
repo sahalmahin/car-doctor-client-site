b193bdb539479b7a51bbcec1dacad8517636590f
sahalmahin <samirsatter0142@gmail.com>
2023-12-12 20:57:00 +0600

    Axios and jsonwebtoken are commonly integrated in client site

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,27 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg';
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import axios from 'axios';
 
 const Login = () => {
 
     const { signIn } = useContext(AuthContext);
+    const location = useLocation();
+    console.log(location);
+    const navigate= useNavigate();
 
     const handleLogin = e => {
         e.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
 
         signIn(email, password)
             .then(result => {
-                const user = result.user;
-                console.log(user);
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                const user = { email };
+                
+                // get access token
+                axios.post('http://localhost:5000/jwt', user, { withCredentials: true })
+                .then(res => {
+                    console.log(res.data);
+                    if(res.data.success){
+                            navigate(location?.state ? location?.state : '/');
+                        }
+                    })
             })
-            .catch(error => {
-                console.log(error.message);
-            })
+            .catch(error => console.log(error.message))
     }
 
     return (
